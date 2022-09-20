9475996dd950ed58c15cac886da126e9e06dc38b
jahkid <31428752+jahkid@users.noreply.github.com>
2022-09-20 14:24:12 +0100

    new commits

--- src/Components/Learn/Learn.js
+++ src/Components/Learn/Learn.js
@@ -1,20 +1,47 @@
 import React from 'react'
+import './Learn.css'
+import img from '../../assets/images_8.png'
+// import Path from '../LearningPath/Path'
 
 function Learn() {
   return (
     <div className='Learn'>
-        <p>Hi, Dee</p>
-        <h2>What will you learn today?</h2>
-        <div>
-            <img src='' />
-            <div>
-                <h3>Artificial Intelligence for Marketing</h3>
-                <p>Learn how artificial intelligence in reshaping 
-                    the way marketing is done at both large and small organizations</p>
-                <button>Learn now</button>  
-            </div>
+      <p>Hi, Dee</p>
+      <h2>What will you learn today?</h2>
+      <div className='Current'>
+        <img src={img} />
+        <div className='Course'>
+          <h3 >Introduction to JavaScript.</h3>
+          <p>JavaScript is the world's most popular programming language. 
+            JavaScript is the programming language of the Web.</p>
+          <button>Learn now</button>  
         </div>
+      </div>
+
       
+      <div className='Path-Box'>  
+      <div className='header-title'>
+        <h3>Your learning path</h3>
+        </div>
+        <div className='box-container'>
+        <div className='Box'>
+            <h4>Intro to React</h4>
+            <p>12 hours of video tutorials <br/> 413 Students </p>
+            <img src=''/>
+          </div>
+          <div className='Box'>
+            <h4>Intro to React</h4>
+            <p>12 hours of video tutorials <br/> 413 Students </p>
+            <img src=''/>
+          </div>
+          <div className='Box'>
+            <h4>Intro to React</h4>
+            <p>12 hours of video tutorials <br/>413 Students </p>
+            <img src=''/>
+          </div>
+        </div>
+        
+      </div>
     </div>
   )
 }
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,18 +1,39 @@
 import React from 'react'
 import './Nav.css'
 import User from '../../assets/user.png'
+import {FaHome} from 'react-icons/fa'
+import {GrCertificate, GrFavorite, GrSchedules} from 'react-icons/gr'
+import {FiSettings} from 'react-icons/fi'
+import {IoNewspaperOutline} from 'react-icons/io5'
 
 function Nav() {
   return (
     <div className='Nav'>
           <h2>Elearn.</h2>
-          <a href=''>Home</a>
-          <a href=''>My Courses</a>
-          <a href=''>Favorite</a>
-          <a href=''>Test</a>
-          <a href=''>Certificate</a>
-          <a href=''>Test</a>
-          <a href=''>Settings</a> 
+          <div className='Navlist'>
+            <FaHome/>
+            <a href=''>Home</a>
+          </div>
+          <div className='Navlist'>
+            <GrSchedules/> 
+            <a href=''>My Courses</a>
+          </div>
+          <div className='Navlist'>
+            <GrFavorite/>
+            <a href=''>Favorite</a>
+          </div>
+          <div className='Navlist'>
+            <IoNewspaperOutline/>
+            <a href=''>Test</a>
+          </div>
+          <div className='Navlist'>
+            <GrCertificate/>
+            <a href=''>Certificate</a>
+          </div>
+          <div className='Navlist'>
+            <FiSettings/>
+            <a href=''>Settings</a>
+          </div> 
           <div className='user'>
             <img src={User}/>
             <div>
